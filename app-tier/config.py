c9d6562802a2b7509ae93749697ee8e5219e5da5
PavanThumati <pavanthumati999@gmail.com>
2025-06-20 12:52:56 +0530

    Update config.py

--- app-tier/config.py
+++ app-tier/config.py
@@ -1,4 +1,4 @@
-DB_HOST = "your-db-host"
-DB_USER = "youruser"
-DB_PASSWORD = "yourpass"
-DB_NAME = "yourdbname"
+DB_HOST = "database-3.cotwo8y6mnk3.us-east-1.rds.amazonaws.com"
+DB_USER = "admin"
+DB_PASSWORD = "adminadmin"
+DB_NAME = "userdb"
